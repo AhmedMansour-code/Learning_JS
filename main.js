898bc32de6518857451a043453c2c1c7ca2928ba
AhmedMansour-code <ahmedmansour.info@gmail.com>
2023-03-08 20:16:05 +0200

    Uploading Files

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-document.write ("<h1>Elzero</h1>");
+// Videos from 1-9
+// Assignment 2 
+//document.write ("<h1 style='color:blue;font-size:50px;font-weight:bold;text-align:center;font-family:arial'>Elzero</h1>");
+
+// document.write ("<h1 class='heading'>Elzero</h1>");
+
+// Assignment 3
+/* console.log ("%cElzero %cWeb %cSchool" , "color: red; font-size: 40px;" ,
+"color: green; font-weight: bold; font-size: 40px;",
+"color: white; background-color: blue; font-size: 40px;"); */
+
+// Assignment 4
+/* console.group("Group 1");
+console.log("Message one");
+console.log("Message two");
+console.group("Child Group");
+console.log("Message one");
+console.log("Message two")
+console.group("Grand Child Group");
+console.log("Message one");
+console.log("Message two")
+console.groupEnd();
+console.groupEnd();
+console.groupEnd();
+console.group("Group 2");
+console.log("Message one");
+console.log("Message two"); */
+
+// Assignment 5
+// console.table(["Elzero","Ahmed","Sameh","Aya","Osama"]);
+
+// Assignment 6
+/* console.log("Iam In Console");
+document.write("Iam In Page"); */
+
+// ------------------------------------------------------------
